9d5448c626904d51437b18eb74f627c338db31f8
BOUAREIO <mamadou.bouare.edu@groupe-gema.com>
2025-09-17 04:29:37 +0200

    Message décrivant les modifications

--- backend/tests/init.py
+++ backend/tests/init.py
@@ -6,7 +6,7 @@ import os
 import tempfile
 import pytest
 from app import create_app, db
-from app.models import User, Conversation
+from backend.models import User, Conversation
 
 # Cette ligne permet à pytest de découvrir les tests
 pytest_plugins = ['app.tests.test_auth', 'app.tests.test_chatbot']
